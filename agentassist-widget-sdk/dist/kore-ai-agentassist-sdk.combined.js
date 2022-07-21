b6f3ef99f075682f660b57d61fe5e7ca7a6052bc
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2022-07-21 15:40:46 +0530

    time AMPM format

--- agentassist-widget-sdk/dist/kore-ai-agentassist-sdk.combined.js
+++ agentassist-widget-sdk/dist/kore-ai-agentassist-sdk.combined.js
@@ -83050,7 +83050,7 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                 function processTranscriptData(data, conversationId, botid) {
                     console.log("---- data====", data)
                     let time = new Date();
-                    let timeStr = time.getHours() + ':' + time.getMinutes();
+                    let timeStr = formatAMPM(time);
                     //  ["user_message", { "botId": "st-760d56df-7303-5968-baae-fc4b3a6d5057", "orgId": "o-e8bde714-ea18-5b2a-8d59-648fb1ba49ee", "accountId": "62b400e7e8d0a17ed980fc3f", "type": "text", "conversationId": "c-423773d-0e9b-4510-93c3-5a492e75ae08", "value": "Operator.", "author": { "type": "USER", "id": "u-cb358b24-cc42-5843-9040-d0a61d1f7cc7", "firstName": "Kore", "lastName": "User" }, "event": "user_message", "_id": "ms-3ceba4be-0870-567a-a36d-236c749d4edb" }]
                     let transcriptTab = $(`#scriptContainer .data-contnet`);
                     let transcriptHtml = `
@@ -83066,9 +83066,20 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                     transcriptTab.append(transcriptHtml);
                 }
 
+                function formatAMPM(date) {
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+                    var ampm = hours >= 12 ? 'pm' : 'am';
+                    hours = hours % 12;
+                    hours = hours ? hours : 12; // the hour '0' should be '12'
+                    minutes = minutes < 10 ? '0'+minutes : minutes;
+                    var strTime = hours + ':' + minutes + ' ' + ampm;
+                    return strTime;
+                  }
+
                 function processAgentMessages(data) {
                     let time = new Date();
-                    let timeStr = time.getHours() + ':' + time.getMinutes();
+                    let timeStr = formatAMPM(time);
                     //  ["agent_message",{"botId":"st-760d56df-7303-5968-baae-fc4b3a6d5057","orgId":"o-e8bde714-ea18-5b2a-8d59-648fb1ba49ee","accountId":"62b400e7e8d0a17ed980fc3f","type":"text","conversationId":"c-0ead79b-28a0-422a-ab91-bab8372345e2","value":"One night.","author":{"type":"AGENT","id":"u-0609dd49-12b7-5432-9cc6-afb2f41128fe","firstName":"dev","lastName":"test"},"event":"agent_message","_id":"ms-1b113b73-d52e-5807-a990-5286b79f81a3"}]
                     let dataConetentTabOfTranscript = $('#scriptContainer .data-contnet');
                     let currentBubbleHtml = `
