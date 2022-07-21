40d20caa2c77d6582381d498c370d0049ca7ed50
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2022-07-21 15:37:41 +0530

    current user and agent time stamp

--- agentassist-widget-sdk/dist/kore-ai-agentassist-sdk.combined.js
+++ agentassist-widget-sdk/dist/kore-ai-agentassist-sdk.combined.js
@@ -81764,6 +81764,8 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                         let userInputData = e.data;
                         let agent_assist_request = {
                             'author': {
+                                "firstName": userInputData.author.firstName,
+                                "lastName": userInputData.author.lastName,
                                 "type": userInputData.author.type
                             },
                             'botId': _botId,
@@ -81773,7 +81775,7 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                         if(isCallConversation === 'true') {
                             if(userInputData.author.type === 'USER') {
                                 console.log('event listener USER Message:')
-                                processTranscriptData(userInputData, userInputData.conversationid, _botId);
+                                processTranscriptData(userInputData, userInputData.conversationid, _botId, );
                                 _agentAsisstSocket.emit('agent_assist_request', agent_assist_request);
                             } else {
                                 console.log('event listener AGENT Message:')
@@ -83047,13 +83049,15 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
 
                 function processTranscriptData(data, conversationId, botid) {
                     console.log("---- data====", data)
+                    let time = new Date();
+                    let timeStr = time.getHours() + ':' + time.getMinutes();
                     //  ["user_message", { "botId": "st-760d56df-7303-5968-baae-fc4b3a6d5057", "orgId": "o-e8bde714-ea18-5b2a-8d59-648fb1ba49ee", "accountId": "62b400e7e8d0a17ed980fc3f", "type": "text", "conversationId": "c-423773d-0e9b-4510-93c3-5a492e75ae08", "value": "Operator.", "author": { "type": "USER", "id": "u-cb358b24-cc42-5843-9040-d0a61d1f7cc7", "firstName": "Kore", "lastName": "User" }, "event": "user_message", "_id": "ms-3ceba4be-0870-567a-a36d-236c749d4edb" }]
                     let transcriptTab = $(`#scriptContainer .data-contnet`);
                     let transcriptHtml = `
                         <div class="other-user-bubble">
                             <div class="name-with-time">
                                 <div class="u-name">${data.author.firstName + data.author.lastName}</div>
-                                <div class="u-time">2:29pm</div>
+                                <div class="u-time">${timeStr}</div>
                             </div>
                             <div class="bubble-data" id="userInputMsg">
                                 <div class="b-text">${data.value}</div>
@@ -83063,12 +83067,14 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                 }
 
                 function processAgentMessages(data) {
+                    let time = new Date();
+                    let timeStr = time.getHours() + ':' + time.getMinutes();
                     //  ["agent_message",{"botId":"st-760d56df-7303-5968-baae-fc4b3a6d5057","orgId":"o-e8bde714-ea18-5b2a-8d59-648fb1ba49ee","accountId":"62b400e7e8d0a17ed980fc3f","type":"text","conversationId":"c-0ead79b-28a0-422a-ab91-bab8372345e2","value":"One night.","author":{"type":"AGENT","id":"u-0609dd49-12b7-5432-9cc6-afb2f41128fe","firstName":"dev","lastName":"test"},"event":"agent_message","_id":"ms-1b113b73-d52e-5807-a990-5286b79f81a3"}]
                     let dataConetentTabOfTranscript = $('#scriptContainer .data-contnet');
                     let currentBubbleHtml = `
                     <div class="current-user-bubble">
                     <div class="name-with-time">
-                        <div class="u-time">2:41pm</div>
+                        <div class="u-time">${timeStr}</div>
                         <div class="u-name">You</div>
                     </div>
                     <div class="bubble-data">
