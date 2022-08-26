db0fa63ef3ac6b8a3b15bed68512a88d3b4b9cd2
PrateekSa-Kore <102820158+PrateekSa-Kore@users.noreply.github.com>
2022-08-26 12:15:27 +0530

    STAT-3824 changes

--- agentassist/src/app/services/end-points.service.ts
+++ agentassist/src/app/services/end-points.service.ts
@@ -556,7 +556,7 @@ export class EndPointsService {
     };
 
     this.serviceList['post.importbot.export'] = {
-      endpoint: this.API_SERVER_URL + '/builder/streams/:streamId/export',
+      endpoint: this.API_SERVER_URL + '/builder/streams/:streamId/export?isAgentAssist=true',
       method: 'post'
     };
 
