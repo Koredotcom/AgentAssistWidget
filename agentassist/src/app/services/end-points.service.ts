7cdb1a921119bfc5029425951ba245317c7bc358
PrateekSa-Kore <102820158+PrateekSa-Kore@users.noreply.github.com>
2022-09-01 13:35:50 +0530

    import bot params change

--- agentassist/src/app/services/end-points.service.ts
+++ agentassist/src/app/services/end-points.service.ts
@@ -535,7 +535,7 @@ export class EndPointsService {
     };
 
     this.serviceList['get.importbotstatus'] = {
-      endpoint: this.API_SERVER_URL + '/users/:userId/builder/importBot/status/:importId?isAgentAssist=true',
+      endpoint: this.API_SERVER_URL + '/users/:userId/builder/importBot/status/:importId',
       method: 'get'
     };
 
