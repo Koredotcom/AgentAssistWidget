ebb24810e893bbee0e16b8d6c5071e0a2eb41da1
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2022-10-28 19:00:36 +0530

    search assist component

---
+++ agentassist/src/app/pages/search-assist/search-assist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-assist',
+  templateUrl: './search-assist.component.html',
+  styleUrls: ['./search-assist.component.scss']
+})
+export class SearchAssistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
