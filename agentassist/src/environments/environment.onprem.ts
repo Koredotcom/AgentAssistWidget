32c9d5acf93ad29ce8e2c821f2f9dc947ccddd66
Sridhar Bahatam <sridhar.bahatam@kore.com>
2022-10-31 20:29:36 +0530

    hardcoding api server url

--- agentassist/src/environments/environment.onprem.ts
+++ agentassist/src/environments/environment.onprem.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   tag: "onprem",
-  API_SERVER_URL: 'https://bots.kore.ai',
+  API_SERVER_URL: 'https://dit2-citipbwot-agentassist.kore.ai',
   USE_SESSION_STORE: false,
   ON_PREMISE: true,
   INLINE_MANUAL_SITE_KEY: "8c0da373990588f43a6e20d8d22228ee",
