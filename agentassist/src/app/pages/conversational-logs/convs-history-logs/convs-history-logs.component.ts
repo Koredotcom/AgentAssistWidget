fada00247bb1c09d223c1994ab1087144eab3983
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2022-12-19 14:02:20 +0530

    STAT-5948: hostory logs component files

---
+++ agentassist/src/app/pages/conversational-logs/convs-history-logs/convs-history-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convs-history-logs',
+  templateUrl: './convs-history-logs.component.html',
+  styleUrls: ['./convs-history-logs.component.scss']
+})
+export class ConvsHistoryLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
