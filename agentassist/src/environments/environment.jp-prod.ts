170e84209a3b8498a5c4e5b3fad24e411a976c2e
Pooja G P <pooja.prakash@kore.com>
2023-01-25 10:13:39 +0530

    fix: PLAT-14913

---
+++ agentassist/src/environments/environment.jp-prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  tag:"prod",
+  API_SERVER_URL:'https://smartassist-jp.kore.ai',
+  USE_SESSION_STORE:false,
+  ON_PREMISE: false,
+  //API_SERVER_URL: 'http://192.168.10.101:5000'
+  SUPPORT_API_SERVER_URL: 'https://smartassist-jp.kore.ai',
+  BOT_NAME: "Kore.ai Platform Support Bot",
+  BOT_ID: "st-72138e03-61d4-58c1-bace-21beb50939b8",
+  CLIENT_ID: "cs-8598b4a0-e2b6-50e5-9f48-d8a48b5c8084",
+  CLIENT_SECRET_ID: "+EuqlSA0IxZE7qun8KUlkruMFTYMqsSYH0I12HQQ5LQ=",
+  MIX_PANEL_TOKEN: '047a11cecb5356a4a4d5b3807d280766',
+  ENABLE_MIX_PANEL: false,
+  INLINE_MANUAL_SITE_KEY: "2495f636853f42a5dad1866d6b59b591"
+};
