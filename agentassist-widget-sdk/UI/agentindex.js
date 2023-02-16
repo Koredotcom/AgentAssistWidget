51d3af0e0a52d18960e2f79ae4cfd4288faff54f
Pooja G P <pooja.prakash@kore.com>
2023-02-16 15:56:11 +0530

    fix: a20-433

--- agentassist-widget-sdk/UI/agentindex.js
+++ agentassist-widget-sdk/UI/agentindex.js
@@ -6611,7 +6611,7 @@ window.AgentAssist = function AgentAssist(containerId, _conversationId, _botId, 
                     $(`.customer-feeling-text`).addClass('bottom-95');
                     setTimeout(() => {
                         headerUUids = undefined;
-                        myBotDropdownHeaderUuids = undefined;
+                    //  myBotDropdownHeaderUuids = undefined;
                     }, 100)
                     // headerUUids = undefined;
 		    UnCollapseDropdownForLastElement(lastElementBeforeNewMessage);
