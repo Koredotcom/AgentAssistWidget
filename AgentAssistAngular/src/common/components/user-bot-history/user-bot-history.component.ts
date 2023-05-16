2d13ab44181a598922288b4ded045c7dd3cf249a
SandeepBa-Kore <81405430+SandeepBa-Kore@users.noreply.github.com>
2023-05-16 17:38:29 +0530

    fixed the cros error due to the eAD key in the headers in the chathistory api (#443)

--- AgentAssistAngular/src/common/components/user-bot-history/user-bot-history.component.ts
+++ AgentAssistAngular/src/common/components/user-bot-history/user-bot-history.component.ts
@@ -37,7 +37,6 @@ export class UserBotHistoryComponent implements OnInit, OnDestroy{
         connectionDetails = response;
         headersVal = {
           'Authorization': this.commonService.grantResponseObj?.authorization?.token_type + ' ' + this.commonService.grantResponseObj?.authorization?.accessToken,
-          'eAD': true,
         }
           $.ajax({
             url: `${connectionDetails.agentassisturl}/api/1.1/botmessages/chathistorytoagentassist?botId=${userBotConversationDetails.botId || connectionDetails?.botId}&userId=${userBotConversationDetails?.userId}&sessionId=${userBotConversationDetails?.sessionId}&limit=-1&msgDirection=true`,
