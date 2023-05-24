ab530e3ef7dab98e9c7cad41728d21cc5e1f9c74
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2023-05-24 19:32:40 +0530

    removed the length check for the interruption

--- AgentAssistAngular/src/assist-tab/components/assist/assist.component.ts
+++ AgentAssistAngular/src/assist-tab/components/assist/assist.component.ts
@@ -437,7 +437,7 @@ export class AssistComponent implements OnInit {
     this.smallTalkOverrideBtnId = null;
     let isTemplateRender = false;
     data = this.commonService.confirmationNodeRenderDataTransform(data);
-    if(this.commonService.isAutomationOnGoing && this.dropdownHeaderUuids && data.suggestions?.length > 0) {
+    if(this.commonService.isAutomationOnGoing && this.dropdownHeaderUuids && data.suggestions) {
       this.dialogTerminatedOrIntruppted();
     }
     if (!this.commonService.isAutomationOnGoing && !this.proactiveModeStatus) {
