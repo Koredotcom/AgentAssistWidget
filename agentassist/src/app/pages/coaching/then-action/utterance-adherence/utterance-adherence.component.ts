6633cb477cb6e28c31f6bb5f5aa148c3af5a2967
saradadh-Kore <sarada.dhana@kore.com>
2023-05-26 16:40:43 +0530

    SACI-45: openai integration done

--- agentassist/src/app/pages/coaching/then-action/utterance-adherence/utterance-adherence.component.ts
+++ agentassist/src/app/pages/coaching/then-action/utterance-adherence/utterance-adherence.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { OpenAIService } from '../../open-ai.service';
 @Component({
   selector: 'app-utterance-adherence',
   templateUrl: './utterance-adherence.component.html',
@@ -11,9 +13,45 @@ export class UtteranceAdherenceComponent implements OnInit {
   color: ThemePalette = 'primary';
   mode: ProgressSpinnerMode = 'determinate';
   value = 50;
-  constructor() { }
+
+  isGeneratingText : boolean = null;
+  openAiUtteranceArray : any = [];
+  utteranceText : string;
+
+
+  @ViewChild(NgbDropdown, { static: true })
+  public utteranceDropdown: NgbDropdown;
+
+  // @ViewChild('utteranceDropdown') utteranceDropdown:NgbDropdown;
+
+  constructor(private openAIService : OpenAIService,
+    private cdRef:ChangeDetectorRef) { }
 
   ngOnInit(): void {
+  }
+
+  getSuggestion(type: string) {
+    this.isGeneratingText = false;
+    this.openAiUtteranceArray = [];
+    this.openAIService.generateText(this.utteranceText, type)
+      .then((res: any) => {
+        res = res.replace(/\n/g,'&&&');
+        if (res) {
+          this.openAiUtteranceArray = Object.assign([], res.split('&&&'));
+          this.openAiUtteranceArray = this.openAiUtteranceArray.filter(function(e){return e}); 
+        };
+        this.isGeneratingText = true;
+      })
+      .catch(err => {
+        this.isGeneratingText = true;
+      })
+  }
+
+  clearAIsuggestions(){
+    this.utteranceText = '';
+    this.isGeneratingText = false;
+    this.openAiUtteranceArray = [];
+    this.cdRef.detectChanges();
   }
 
   closeAdherence(e?) {
