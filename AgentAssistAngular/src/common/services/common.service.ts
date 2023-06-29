93f89461539b7e83ad75a20b24ccaac8485c303b
SandeepBa-Kore <81405430+SandeepBa-Kore@users.noreply.github.com>
2023-06-29 18:49:14 +0530

    Iid headers widget (#495) (#514)
    * IID changes made in widget
    
    * resolved syntax error
    
    ---------
    
    Co-authored-by: saradadh-Kore <sarada.dhana@kore.com>

--- AgentAssistAngular/src/common/services/common.service.ts
+++ AgentAssistAngular/src/common/services/common.service.ts
@@ -693,7 +693,10 @@ export class CommonService {
   async renderHistoryFeedBack(url) {
     const response = await $.ajax({
       method: 'GET',
-      url: url
+      url: url,
+      headers :  {
+        'iid' : this.configObj.botId ? this.configObj.botId : 'st-1c3a28c8-335d-5322-bd21-f5753dc7f1f9'
+    }
     });
     if (response.results) {
       return response.results;
@@ -819,7 +822,7 @@ export class CommonService {
   }
 
   async getAgentHistoryData(url = '', data = {}) {
-    let headersVal = {};
+    let headersVal : any = {};
     if(this.configObj.fromSAT) {
         headersVal = {
             'Authorization': 'bearer' + ' ' + this.configObj.token,
@@ -831,6 +834,7 @@ export class CommonService {
             'Authorization': this.grantResponseObj?.authorization.token_type + ' ' + this.getAccessToken()
         }
     }
+    headersVal.iid = this.configObj.botId ? this.configObj.botId : 'st-1c3a28c8-335d-5322-bd21-f5753dc7f1f9'
     const response = await $.ajax({
         method: 'GET',
         url: url,
