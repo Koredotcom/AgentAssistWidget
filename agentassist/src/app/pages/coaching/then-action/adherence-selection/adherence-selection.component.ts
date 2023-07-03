3d79687fccadc0f341ddb7c31ac11a2b244ee218
saradadh-Kore <108404503+saradadh-Kore@users.noreply.github.com>
2023-07-03 13:17:56 +0530

    SACI-549:utterance delete and save button disable issue fixed (#521)

--- agentassist/src/app/pages/coaching/then-action/adherence-selection/adherence-selection.component.ts
+++ agentassist/src/app/pages/coaching/then-action/adherence-selection/adherence-selection.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServiceInvokerService } from '@kore.services/service-invoker.service';
 import { SliderComponentComponent } from 'src/app/shared/slider-component/slider-component.component';
 import { COACHINGCNST } from '../../coaching.cnst';
 import { CoachingService } from '../../coaching.service';
@@ -14,6 +15,7 @@ export class AdherenceSelectionComponent implements OnInit {
   @Input() adherenceForm: FormGroup;
   @Input() createOrEdit: string = '';
   @Input() adherenceSelection : string;
+  @Input() adhereClick : boolean;
   @Output() deleteAdherence = new EventEmitter();
 
   openSetUtterance : boolean = false;
@@ -37,7 +39,8 @@ export class AdherenceSelectionComponent implements OnInit {
 
   constructor(private cdRef : ChangeDetectorRef,
     private coachingService : CoachingService,
-    private fb : FormBuilder) { }
+    private fb : FormBuilder,
+    private service : ServiceInvokerService) { }
 
   ngOnChanges(changes : any){
     if(changes?.createOrEdit?.currentValue === COACHINGCNST.EDIT){
@@ -54,7 +57,7 @@ export class AdherenceSelectionComponent implements OnInit {
       this.selectedMinutes = COACHINGCNST.SELECTED_ACTION_MINS;
       this.selectedMessages = COACHINGCNST.SELECTED_ACTION_MESSAGES;
     }
-    if(this.adherenceSelection){
+    if(this.adhereClick && this.adherenceSelection){
       this.clickOnAdherenceSelection(this.adherenceSelection);
     }
   }
@@ -81,8 +84,33 @@ export class AdherenceSelectionComponent implements OnInit {
   deleteAdherenceClick(){
     // this.clickOnAdherenceSelection(null);
     this.adherenceForm.removeControl('adherence');
-    this.adherenceForm.addControl('adherence',this.fb.group(this.coachingService.getAdherenceForm()))
-    this.deleteAdherence.emit(true);
+    this.adherenceForm.addControl('adherence',this.fb.group(this.coachingService.getAdherenceForm(this.selectedAdherenceType)));
+    if(this.selectedAdherenceType == this.coachingConst.UTTERANCE){
+      this.deleteUtterances();
+    }else{
+      this.deleteAdherence.emit(false);
+    }
+  }
+
+  deleteUtterances(){
+    let utterIds : any = [];
+    this.service.invoke("get.agentcoachingutteranceByRef",
+    {
+      refId: this.adherenceForm.value._id,
+    }).subscribe((data) => {
+      
+      if(data?.length > 0){
+        data.forEach(element => {
+          utterIds.push(element._id);
+        });        
+        if(utterIds.length > 0){
+          (this.adherenceForm.controls.adherence as FormGroup).controls?.deleteUtterances?.setValue([...utterIds]);
+         
+        }
+      }
+      
+      this.deleteAdherence.emit(utterIds.length);
+    });
   }
 
   onEnterMinOrMessages(e){
@@ -115,11 +143,11 @@ export class AdherenceSelectionComponent implements OnInit {
       if(type == COACHINGCNST.UTTERANCE){
 
         (<FormGroup>this.adherenceForm.controls.adherence).addControl('session', new FormControl(this.selectedSession));
-        (<FormGroup>this.adherenceForm.controls.adherence).addControl('utteranceCount', new FormControl(''));
-        setTimeout(() => {
-          (this.adherenceForm.controls?.adherence as FormGroup)?.controls['utteranceCount'].setValidators(Validators.required);
-          (this.adherenceForm.controls?.adherence as FormGroup)?.controls['utteranceCount'].updateValueAndValidity();
-        }, 100);
+        (<FormGroup>this.adherenceForm.controls.adherence).addControl('utteranceCount', new FormControl('',[Validators.required]));
+        // setTimeout(() => {
+        //   (this.adherenceForm.controls?.adherence as FormGroup)?.controls['utteranceCount'].setValidators(Validators.required);
+        //   (this.adherenceForm.controls?.adherence as FormGroup)?.controls['utteranceCount'].updateValueAndValidity();
+        // }, 100);
       };
     }
     (this.adherenceForm.controls?.adherence as FormGroup)?.controls?.adType.setValue(type);
