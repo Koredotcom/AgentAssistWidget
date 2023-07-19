cc65b7297362e2f08c2e57d423828e2560fdcd4b
sathishra-Kore <117149117+sathishra-Kore@users.noreply.github.com>
2023-07-19 12:28:08 +0530

    Call controlls (#536)
    * SACI-654 design changes completed
    
    * SACI-675: Added call control parameters
    
    * SACI-675: Added call control parameters
    
    * SACI-675: modified call control params
    
    * SACI-675: modified call control params
    
    * SACI-675: modified call control params
    
    * SACI-675
    
    * SACI-675
    
    ---------
    
    Co-authored-by: devendarba-kore <devendar.bala@kore.com>

--- agentassist/src/app/pages/settings/adv-settings/adv-settings.component.ts
+++ agentassist/src/app/pages/settings/adv-settings/adv-settings.component.ts
@@ -136,7 +136,7 @@ export class AdvSettingsComponent implements OnInit, OnDestroy {
       this.showVPSlider = false;
     }
     if (this.incomingSetupConfigured) {
-      this.vpSlider.openSlider("#voicePreferenace", "width940");
+      this.vpSlider.openSlider("#voicePreferenace", "width550");
       this.showVPSlider = true;
     }
   }
@@ -167,5 +167,8 @@ export class AdvSettingsComponent implements OnInit, OnDestroy {
     this.destroyed$.next();
     this.destroyed$.complete();
     this.subs.unsubscribe();
+  }
+  getNewData(){
+    this.getAdvSettings()
   }
 }
