11857fbd43d2a08c6bfcd5b56a24fc2fb1529d67
saradadh-Kore <sarada.dhana@kore.com>
2023-07-26 17:55:34 +0530

    added checklist form

--- agentassist/src/app/pages/guided-checklist/common/checklist-create/checklist-create.component.ts
+++ agentassist/src/app/pages/guided-checklist/common/checklist-create/checklist-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@kore.services/auth.service';
+import { LocalStoreService } from '@kore.services/localstore.service';
+import { workflowService } from '@kore.services/workflow.service';
 import { SliderComponentComponent } from 'src/app/shared/slider-component/slider-component.component';
 
 @Component({
@@ -10,12 +14,47 @@ export class ChecklistCreateComponent implements OnInit {
 
   @Output() closeCheckList = new EventEmitter();
   @ViewChild('checklistCreateSlider', { static: true }) checklistCreateSlider: SliderComponentComponent;
+  
+  checkListForm : FormGroup;
+  stageForm : FormGroup;
+  selAcc = this.local.getSelectedAccount();
+  
 
 
-
-  constructor() { }
+  constructor( private fb: FormBuilder,
+    private auth: AuthService, 
+    private local: LocalStoreService,
+    private workflowService: workflowService ) { }
 
   ngOnInit(): void {
+  }
+
+  createCheckListForm(){
+    this.checkListForm = new FormGroup(
+      { 
+        "botId": new FormControl(this.auth.isLoadingOnSm && this.selAcc ? this.selAcc['instanceBots'][0]?.instanceBotId : this.workflowService.getCurrentBt(true)._id, [Validators.required]),
+        "name": new FormControl('',[Validators.required]),
+        "description": new FormControl(''),
+        "type" : new FormControl('', [Validators.required]),
+        "status" : new FormControl('',[Validators.required]),
+        "triggerSetting" : new FormGroup({
+          'allinteractions' : new FormControl(false),
+          'selectedAgent' : this.fb.array([]),
+          'selectedQueues' : this.fb.array([]),
+          'selectedSkills' : this.fb.array([]),
+        }),
+        "stages": this.fb.array([], [Validators.required])
+      }
+    );
+  }
+
+  createGroupForm(){
+    this.stageForm = new FormGroup({
+      "botId": new FormControl(this.auth.isLoadingOnSm && this.selAcc ? this.selAcc['instanceBots'][0]?.instanceBotId : this.workflowService.getCurrentBt(true)._id, [Validators.required]),
+      "name": new FormControl('',[Validators.required]),
+      "color" : new FormControl(''),
+      "triggerBy" : new FormControl('')
+    })
   }
 
   closeCheckListScreen(){
