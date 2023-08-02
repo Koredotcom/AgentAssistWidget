e7de9cafbed1b7fac7ef356f0944b1ff7b7daec1
devendarba-kore <devendar.bala@kore.com>
2023-08-02 21:17:10 +0530

    SACI-755 stage create step create styles updated

--- agentassist/src/app/pages/guided-checklist/guided-checklist.module.ts
+++ agentassist/src/app/pages/guided-checklist/guided-checklist.module.ts
@@ -4,11 +4,11 @@ import { GuidedChecklistComponent } from './guided-checklist/guided-checklist.co
 import { RouterModule, Routes } from '@angular/router';
 import { PrimaryChecklistComponent } from './primary-checklist/primary-checklist.component';
 import { DynamicChecklistComponent } from './dynamic-checklist/dynamic-checklist.component';
-import { ChecklistCreateComponent } from './common/checklist-create/checklist-create.component';
-import { StageCreateComponent } from './common/stage-create/stage-create.component';
 import { StepCreateComponent } from './common/step-create/step-create.component';
 import { ChecklistService } from './checklist.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ChecklistCreateComponent } from './common/stages-list/checklist-create.component';
+import { StageCreateComponent } from './common/create-checklist/stage-create.component';
 
 const routes: Routes = [
   { path: '', component:  GuidedChecklistComponent}
