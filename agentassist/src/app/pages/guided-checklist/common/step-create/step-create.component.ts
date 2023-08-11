2f3444e3bcf38282005b4fedf75c92553cd8076d
saradadh-Kore <sarada.dhana@kore.com>
2023-08-11 18:29:37 +0530

    step configure

--- agentassist/src/app/pages/guided-checklist/common/step-create/step-create.component.ts
+++ agentassist/src/app/pages/guided-checklist/common/step-create/step-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { CHECKLISTCNST } from '../checklist.const';
 
 @Component({
   selector: 'app-step-create',
@@ -12,7 +13,57 @@ export class StepCreateComponent implements OnInit, OnChanges {
   @Input() stepForm: FormGroup;
   @Input() createOrUpdateStep = 'create';
   @Output() updateStep = new EventEmitter();
+
+  checklistConst = CHECKLISTCNST.COLORS;
+
   showButtons = false;
+  selectedStepButton : any = '';
+  selectedRunDialog : any = '';
+
+  stepButtons : any = {
+    'confirmation' : "Confirmation:Yes/No",
+    'runDialog' : "Run Dialog"
+  }
+
+  selectedRunColorCodeKey : any = '#0BA5EC';
+  selectedRunColorCodeValue : any = '#F0F9FF';
+
+  stepConfirmationButtonList : any = [
+    {
+      name : 'yes',
+      color : '#F0F9FF'
+    },
+    {
+      name : 'no',
+      color : '#F0F9FF'
+    }
+  ]
+
+ 
+  selectedConfirmationColorCode : any = {
+    'yes' : '#0BA5EC',
+    'no' : '#0BA5EC'
+  };
+  
+
+  runDialogList : any = [
+    {
+      name : 'App Solution'
+    },
+    {
+      name : 'Discount'
+    },
+    {
+      name : 'Customer Onboarding'
+    },
+    {
+      name : 'Sales'
+    },
+    {
+      name : 'Laptop Recommendation'
+    }
+  ]
+
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -33,5 +84,20 @@ export class StepCreateComponent implements OnInit, OnChanges {
 
   update(){
     this.updateStep.next(true);
+  }
+
+  changeStepButton(key){
+    this.selectedStepButton = key;
+  }
+
+  colorUpdate(confirmationNode, color) {
+   this.selectedConfirmationColorCode[confirmationNode.name] = color;
+   
+  }
+
+  colorUpdateRun(color){    
+    this.selectedRunColorCodeKey = color.key;
+    this.selectedRunColorCodeValue = color.value;
+    
   }
 }
