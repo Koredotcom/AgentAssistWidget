55a5b473a73c04f7b078221f79810e1bdc88178e
sathish rambathini <sathish.rambathini@kore.com>
2023-08-24 15:33:19 +0530

    guided-checklist

---
+++ AgentAssistAngular/src/assist-tab/components/checklists/checklists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checklists',
+  templateUrl: './checklists.component.html',
+  styleUrls: ['./checklists.component.scss']
+})
+export class ChecklistsComponent {
+
+}
