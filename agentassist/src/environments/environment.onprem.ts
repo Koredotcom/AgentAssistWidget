b1587c2636efef79610eda6b8adaf0bfdec77ae5
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2023-09-07 12:04:04 +0530

    STAT-3818: removed the agentassist widget sdk folder and updated the env files

--- agentassist/src/environments/environment.onprem.ts
+++ agentassist/src/environments/environment.onprem.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   tag: "onprem",
-  API_SERVER_URL: 'https://dev-agentassist.kore.ai',
+  API_SERVER_URL: 'https://huddl-agentassist-dev.kore.ai',
   USE_SESSION_STORE: false,
   ON_PREMISE: true,
   INLINE_MANUAL_SITE_KEY: "8c0da373990588f43a6e20d8d22228ee",
