6c367ebe567f8581dda3c2c60d659f7ebae19825
sathishra-Kore <117149117+sathishra-Kore@users.noreply.github.com>
2023-10-12 10:54:52 +0000

    change

--- AgentAssistAngular/src/app/app-routing.module.ts
+++ AgentAssistAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'UI/agentassist-iframe.html', component : AppComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
