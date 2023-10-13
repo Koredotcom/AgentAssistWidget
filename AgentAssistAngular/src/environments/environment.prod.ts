0b434354acc25005374051ec9747f6d041ae4842
sathish rambathini <sathish.rambathini@kore.com>
2023-10-13 12:25:09 +0530

    STAT-10416

--- AgentAssistAngular/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    tag:"dev",
-    API_SERVER_URL:'https://dev-smartassist.kore.ai',
-    SUPPORT_API_SERVER_URL:'https://uat-smartassist.kore.ai'
-  };
-  
