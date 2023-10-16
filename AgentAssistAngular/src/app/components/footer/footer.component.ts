50efc4f05018ee54d678f8e40fd901003e9ef66c
devendarba-kore <devendar.bala@kore.com>
2023-10-16 14:40:26 +0530

    intial commite and added bootstarp styles

--- AgentAssistAngular/src/app/components/footer/footer.component.ts
+++ AgentAssistAngular/src/app/components/footer/footer.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, TemplateRef } from '@angular/core';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  constructor(private offcanvasService: NgbOffcanvas){
 
+  }
+  openEnd(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { position: 'bottom' });
+	}
 }
