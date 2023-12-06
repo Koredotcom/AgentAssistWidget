886712094fc86c05f4e86dd1f2d4918db57beaeb
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2023-12-06 18:47:42 +0530

    Testing the url issue

--- agentassist/src/environments/environment.onprem.ts
+++ agentassist/src/environments/environment.onprem.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   tag: "onprem",
-  API_SERVER_URL: 'https://huddl-agentassist-dev.kore.ai',
+  API_SERVER_URL: 'https://staging-agentassist.kore.ai',
   USE_SESSION_STORE: false,
   ON_PREMISE: true,
   INLINE_MANUAL_SITE_KEY: "8c0da373990588f43a6e20d8d22228ee",
