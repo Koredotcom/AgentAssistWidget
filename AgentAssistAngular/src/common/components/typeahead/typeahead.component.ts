f096077405501bbad9228fed3c7763392faa4359
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2023-12-08 14:43:07 +0530

    removed the static value and console log

--- AgentAssistAngular/src/common/components/typeahead/typeahead.component.ts
+++ AgentAssistAngular/src/common/components/typeahead/typeahead.component.ts
@@ -35,7 +35,6 @@ export class TypeaheadComponent implements OnInit {
     //this.isArryDataSet = Array.isArray(this.dataSet);
   }
   ngOnInit(){        
-    console.log('this.connectionDetails', this.connectionData)
     this.subScriberEvent();
   }
   ngOnDestroy() {
@@ -98,14 +97,14 @@ export class TypeaheadComponent implements OnInit {
                 'Authorization': 'bearer' + ' ' + connectionDetails.token,
                 'AccountId': connectionDetails.accountId,
                 'eAD': false,
-                'iid' : connectionDetails.botId ? connectionDetails.botId : 'st-1c3a28c8-335d-5322-bd21-f5753dc7f1f9'
+                'iid' : connectionDetails.botId ? connectionDetails.botId : ''
             }
         } else {
             headersVal = {
                 'Authorization': this.commonService.grantResponseObj?.authorization?.token_type + ' ' + this.commonService.grantResponseObj?.authorization?.accessToken,
                 "AccountId": this.commonService.grantResponseObj?.userInfo?.accountId,
                 'eAD': true,
-                'iid' : connectionDetails.botId ? connectionDetails.botId : 'st-1c3a28c8-335d-5322-bd21-f5753dc7f1f9'
+                'iid' : connectionDetails.botId ? connectionDetails.botId : ''
             }
        }
         if (value?.length > 0 && this.aaSettings?.searchAssistConfig?.showAutoSuggestions) {        
