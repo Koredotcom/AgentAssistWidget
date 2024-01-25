ed7bb61d7ef3855232e42d3ce032794465d44c14
sathish rambathini <sathish.rambathini@kore.com>
2024-01-25 20:58:01 +0530

    PLAT-25448

--- AgentAssistAngular/src/assist-tab/services/assist.service.ts
+++ AgentAssistAngular/src/assist-tab/services/assist.service.ts
@@ -384,14 +384,14 @@ export class AssistService {
         </div>
         <div class="action-links">
             <button class="send-run-btn" id="run-${uuids + index}" data-child-bot-id="${ele?.childBotId}" data-child-bot-name="${ele?.childBotName}"
-            data-dialog-run='${JSON.stringify(ele)}' data-traits="${JSON.stringify(data?.traits || [])}">RUN</button>
+            data-dialog-run='${JSON.stringify(ele)}' data-traits='${JSON.stringify(data?.traits || [])}'>RUN</button>
             <div class="elipse-dropdown-info" id="showRunForAgentBtn-${uuids + index}">
                 <div class="elipse-icon" id="elipseIcon-${uuids + index}">
                     <i class="ast-overflow" id="overflowIcon-${uuids + index}"></i>
                 </div>
                 <div class="dropdown-content-elipse" id="runAgtBtn-${uuids + index}" data-dialog-run='${JSON.stringify(ele)}'>
                     <div class="list-option" id="agentSelect-${uuids + index}" data-child-bot-id="${ele?.childBotId}" data-child-bot-name="${ele?.childBotName}"
-                    data-dialog-run='${JSON.stringify(ele)}' data-traits="${JSON.stringify(data?.traits || [])}">Run with Agent Inputs</div>
+                    data-dialog-run='${JSON.stringify(ele)}' data-traits='${JSON.stringify(data?.traits || [])}'>Run with Agent Inputs</div>
                 </div>
         </div>
     </div>`;
