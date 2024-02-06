89287585a0ba099a356077662b933a8612006139
sathish rambathini <sathish.rambathini@kore.com>
2024-02-06 16:33:01 +0530

    STAT-13079

--- AgentAssistAngular/src/app/services/web-socket.service.ts
+++ AgentAssistAngular/src/app/services/web-socket.service.ts
@@ -199,11 +199,11 @@ export class WebSocketService implements OnDestroy {
       }
       this.handleIsSendWelcomeRequest(data);
       let settimeoutTime = this.getTimeout();
-      setTimeout(() => {
+      // setTimeout(() => {
         console.log("🚀 ~ WebSocketService ~ this._agentAsisstSocket.on ~ settimeoutTime:", settimeoutTime)
         this.agentAssistResponse$.next(data);
         this.addOrRemoveLoader(false);
-      }, settimeoutTime);
+      // }, settimeoutTime);
     });
 
     this._agentAsisstSocket.on(EVENTS.agent_menu_response, (data : any) => {
