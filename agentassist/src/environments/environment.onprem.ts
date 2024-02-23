7a1b0eaa4672afb4ff8c1be40fbe7f0dd0acc3b4
KI0677\Sandeep.Batta <sandeep.batta@kore.com>
2024-02-23 12:38:19 +0530

    url change

--- agentassist/src/environments/environment.onprem.ts
+++ agentassist/src/environments/environment.onprem.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
+  production: false,
   tag: "onprem",
-  API_SERVER_URL: 'https://huddl-agentassist-dev.kore.ai',
+  API_SERVER_URL: 'https://huddl-smartassist-dev.kore.ai',
   USE_SESSION_STORE: false,
   ON_PREMISE: true,
   INLINE_MANUAL_SITE_KEY: "8c0da373990588f43a6e20d8d22228ee",
